b1ab6f0d6d80b2ac736504091f5398d3261c826c
AbdulManan505 <115353904+AbdulManan505@users.noreply.github.com>
2022-10-11 17:21:56 +0500

    Create batatype.java

---
+++ batatype.java
@@ -0,0 +1,14 @@
+public class uniHW2 {
+	public static void main(String[] args) {
+		int myNum = 5;               // Integer (whole number)
+		float myFloatNum = 5.99f;    // Floating point number
+		char myLetter = 'D';         // Character
+		boolean myBool = true;       // Boolean
+		String myText = "Hello";     // String
+		System.out.println("Int is " + myNum);
+		System.out.println("Float is " + myFloatNum);
+		System.out.println("Char is " + myLetter);
+		System.out.println("Boolean is " + myBool);
+		System.out.println("string is " + myText);
+	}
+}
